c7581d26b373f5cf5c414e68ed409783728dfe82
DorukSega Yılmaz <dorukyogscast@gmail.com>
2021-03-24 22:38:55 +0300

    Completed most of the visuals

--- js/main.js
+++ js/main.js
@@ -1,4 +1,34 @@
+var isdeploying=false;
+var iscombating=false;
+var isshopping=false;
+function error(nmbr){
+    console.log("Error "+nmbr);
+}
 function load(){
+    console.log(getcardadjent(1))
 }
-window.onresize = function() {
+function gamestart() { 
+    //Shuffle Phase
+    //Deploy Phase
+    //Combat Phase
+    //Shop Phase
+
 }
+ function getemptycardslots(){
+
+ }
+ function getcardadjent(slotnumber) { 
+    if (slotnumber>1 & slotnumber<7 ){
+        var slots = [(slotnumber-1),(slotnumber+1)]
+        return  slots;
+    } else if(slotnumber == 7){
+        var slots = [6]
+        return  slots;
+    } else if(slotnumber == 1){
+        var slots = [2]
+        return  slots;
+    } else{
+        error("slot number for getcardadjent is wrong")
+        return 0;
+    }
+  }
