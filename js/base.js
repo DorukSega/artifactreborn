aa6fbb1dfa4df097dd857d5c453338599f7cee03
Doruk Yılmaz <dorukyogscast@gmail.com>
2021-04-11 13:55:47 +0300

    more

--- js/base.js
+++ js/base.js
@@ -28,28 +28,6 @@ function preload(url) {
   //img.style.display="none";
   body.appendChild(img);
  }
- function preloadall(){
-  var img_to_load = [];
-  img_to_load=img_to_load.concat(getallcardarts());
-  //console.log(img_to_load);
-  var loaded_images = 0;
-  for (var i=0; i<img_to_load.length; i++) {
-      var img = document.createElement('img');
-      img.src = img_to_load[i];
-      img.style.display = 'hidden'; // don't display preloaded images
-      img.onload = function () {
-          loaded_images ++;
-          if (loaded_images == img_to_load.length) {
-            $(".gamearea").show();
-            gameload();
-          }
-          else {
-             // console.log((100*loaded_images/img_to_load.length) + '% loaded');
-          }
-      }
-      document.body.appendChild(img);
-  }
- }
 function getRndInteger(min, max) {
     var maxreal = parseInt(max) + 1  
     return Math.floor(Math.random() * (maxreal - parseInt(min))) + parseInt(min);
@@ -209,5 +187,5 @@ function Iplay(){
   $(".P").hide();
   $(".S").hide();
   $(".menupage").hide();
-  preloadall()
+  preloadall();
 }
--- js/visual_actions.js
+++ js/visual_actions.js
@@ -29,6 +29,28 @@ function load(){
     console.log("Checksum: "+ checksum)
     $(".Mversion").text(gamename+" - "+version+"\n\n"+"Checksum: "+checksum);
 }
+function preloadall(){
+    var img_to_load = [];
+    img_to_load=img_to_load.concat(getallcardarts());
+    //console.log(img_to_load);
+    var loaded_images = 0;
+    for (var i=0; i<img_to_load.length; i++) {
+        var img = document.createElement('img');
+        img.src = img_to_load[i];
+        img.style.display = 'hidden'; // don't display preloaded images
+        img.onload = function () {
+            loaded_images ++;
+            if (loaded_images == img_to_load.length) {
+              $(".gamearea").show();
+              gameload();
+            }
+            else {
+               // console.log((100*loaded_images/img_to_load.length) + '% loaded');
+            }
+        }
+        document.body.appendChild(img);
+    }
+   }
 function gameload() { 
     aiversusgamerules() //for now
     drawcard("Legion Standard Bearer");
